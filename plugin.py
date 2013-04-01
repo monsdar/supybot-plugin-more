b21c9d891267a2839ee309bf2ea9e24e7d786faa
monsdar <monsdar@3701e02c-1a8b-bbe1-992e-0b21c9682cf4>
2013-04-01 13:54:33 +0000

    Initial checkin

---
+++ plugin.py
@@ -0,0 +1,23 @@
+###
+# Copyright (c) 2013, Nils Brinkmann
+# All rights reserved.
+#
+#
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+
+
+class More(callbacks.Plugin):
+    """Displays generic information about a specific topic."""
+    pass
+
+
+Class = More
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
